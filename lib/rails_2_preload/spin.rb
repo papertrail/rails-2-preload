767a77af86708347df23c0ec329b2dbdb9d26098
Aaron Quint <aaron@quirkey.com>
2013-05-09 17:21:07 -0400

    Fix tests

--- lib/rails_2_preload/spin.rb
+++ lib/rails_2_preload/spin.rb
@@ -3,16 +3,15 @@ require 'rails_2_preload'
 module Rails2Preload
   module Spin
 
-    # By default, we'll preload until the application classes (i.e. models) are
-    # loaded. This is optimal for unit testing.
-    Rails2Preload.preload_until(:load_application_classes)
-
     # Called from .spin.rb to add all hooks necessary to integrate
     # Rails2Preload.
     def self.add_spin_hooks
-      Spin.hook(:before_preload) { Rails2Preload.prepare_rails }
+      # By default, we'll preload until the application classes (i.e. models) are
+      # loaded. This is optimal for unit testing.
+      Rails2Preload.preload_until(:load_application_classes)
+      ::Spin.hook(:before_preload) { Rails2Preload.prepare_rails }
 
-      Spin.hook(:after_preload) do
+      ::Spin.hook(:after_preload) do
         if Rails2Preload.preload_methods.include? :load_application_classes
           # If classes are preloaded, empty the connection pool so forked
           # processes are forced to establish new connections. Otherwise, the
@@ -22,10 +21,10 @@ module Rails2Preload
         end
       end
 
-      Spin.hook(:after_fork) do
+      ::Spin.hook(:after_fork) do
         # Create a new initializer instance, but reuse the configuration already
         # established by the preload phase.
-        initializer = Rails::Initializer.new(Rails.configuration)
+        initializer = ::Rails::Initializer.new(::Rails.configuration)
         initializer.postload
       end
     end

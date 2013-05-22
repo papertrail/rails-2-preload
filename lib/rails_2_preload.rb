66636b184f2864ceb2e0e651ea1e0d2d54289f0e
Aaron Quint <aaron@quirkey.com>
2013-05-22 17:54:42 -0400

    Remove duplicated method

--- lib/rails_2_preload.rb
+++ lib/rails_2_preload.rb
@@ -73,29 +73,6 @@ module Rails2Preload
   # loaded. This is optimal for unit testing.
   self.preload_until(:disable_dependency_loading)
 
-  # Called from .spin.rb to add all hooks necessary to integrate
-  # Rails2Preload.
-  def self.add_spin_hooks
-    Spin.hook(:before_preload) { Rails2Preload.prepare_rails }
-
-    Spin.hook(:after_preload) do
-      if Rails2Preload.preload_methods.include? :load_application_classes
-        # If classes are preloaded, empty the connection pool so forked
-        # processes are forced to establish new connections. Otherwise, the
-        # second time a test is run, an exception like this is raised:
-        #   ActiveRecord::StatementInvalid PGError: no connection to the server
-        ActiveRecord::Base.connection_pool.disconnect!
-      end
-    end
-
-    Spin.hook(:after_fork) do
-      # Create a new initializer instance, but reuse the configuration already
-      # established by the preload phase.
-      initializer = Rails::Initializer.new(Rails.configuration)
-      initializer.postload
-    end
-  end
-
   # Called by the Rails patch to run the preload phase.
   def self.preload(initializer)
     preload_methods.each { |method| benchmark(initializer, method) }
